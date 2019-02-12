eed107d26103deb7757cd098287b7a53f3645920
Alec <avan-etten@mail.greenriver.edu>
2019-02-12 02:13:56 -0500

    added select for pet owners and pets

--- index.php
+++ index.php
@@ -87,3 +87,17 @@ $statement->bindParam(':pet', $pet, PDO::PARAM_STR);
 $statement->bindParam(':new', $new, PDO::PARAM_STR);
 //Execute
 $statement->execute();
+
+//Define the query
+$sql="SELECT * FROM petOwners INNER JOIN pets ON petOwners.petId = pets.id";
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+//Execute the statement
+$statement->execute();
+//Process the result
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo "<table>";
+foreach($result as $row) {
+    echo "<tr><td>".$row['first']." ".$row['last']."</td><td>".$row['name'] . "</td><td>" . $row['color']. "</td></tr>";
+}
+echo "</table>";
